5f8e6e8fad41658ebd8a281fe1f5c76ba013f344
Ionut Bostan <bostan.ionut@gmail.com>
2022-09-16 18:11:25 +0200

    Update web h1

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Turborepo test 1</h1>
+      <h1>Vite + React + Turborepo + Dependabot</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
