46727c7d20dcac936e214c690dd4d1b4d8d60117
Ionut Bostan <bostan.ionut@gmail.com>
2022-12-05 19:15:27 +0100

    Upgrade storybook files to the new version

--- apps/docs/.storybook/main.cjs
+++ apps/docs/.storybook/main.cjs
@@ -1,12 +1,19 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": {
-    name: "@storybook/react-vite",
-    options: {}
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  "core": {},
-  "features": {
-    "storyStoreV7": true
-  }
-};
+  core: {},
+  features: {
+    storyStoreV7: true,
+  },
+  docs: {
+    docsPage: 'automatic',
+  },
+}
--- apps/docs/src/stories/Page.tsx
+++ apps/docs/src/stories/Page.tsx
@@ -40,7 +40,7 @@ export const Page: React.VFC = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose
-            such data from the args of child component stories
+            such data from the "args" of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock
@@ -50,7 +50,7 @@ export const Page: React.VFC = () => {
         <p>
           Get a guided tutorial on component-driven development at{' '}
           <a
-            href="https://storybook.js.org/tutorials/"
+            href="renderers/react/template/cli/ts/Page"
             target="_blank"
             rel="noopener noreferrer"
           >
