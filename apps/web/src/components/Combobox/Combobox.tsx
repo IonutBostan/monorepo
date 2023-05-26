b23caf2dfac82ca53d33c1969aa870c84b178a50
Ionut Bostan <bostan.ionut@gmail.com>
2023-05-26 18:02:44 +0200

    Add more data to dropdown

--- apps/web/src/components/Combobox/Combobox.tsx
+++ apps/web/src/components/Combobox/Combobox.tsx
@@ -9,6 +9,7 @@ type Person = {
 
 const people: Person[] = [
   { id: 1, name: 'Wade Cooper' },
+  { id: 1, name: 'Mike Tyson' },
   { id: 2, name: 'Arlene Mccoy' },
   { id: 3, name: 'Devon Webb' },
   { id: 4, name: 'Tom Cook' },
